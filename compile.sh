f0096b1af9233865f37afee49f8c283dd8b1bae2
David Brazdil <db538@zenith.cl.cam.ac.uk>
2013-12-19 15:14:58 +0000

    Added local OpenJDK install to PATH

--- compile.sh
+++ compile.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 WORKING_DIR="work"
-
+export PATH="$PATH":~/apps/openjdk7/usr/local/openjdk7/bin/
+export EXPERIMENTAL_USE_JAVA7=true
 
 cd "$WORKING_DIR"
 . build/envsetup.sh || exit
 lunch aosp_mips-eng || exit
-export EXPERIMENTAL_USE_JAVA7=true
 mmma art
 
 
