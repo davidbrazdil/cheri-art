5a32362d9355e1fdd037dc9757ef1fc6efada63a
David Brazdil <david.brazdil@gmail.com>
2013-12-20 19:48:08 +0000

    Fixed Java PATH

--- compile.sh
+++ compile.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+CHERIBSD_SDK="/home/db538/cheri-sdk/sdk"
+OPENJDK="/home/db538/apps/openjdk7/usr/local/openjdk7/bin/"
+
 WORKING_DIR="work"
-export PATH="$PATH":~/apps/openjdk7/usr/local/openjdk7/bin/
+export PATH="$PATH":"$OPENJDK"
 export EXPERIMENTAL_USE_JAVA7=true
 
 export TARGET_OS=freebsd
 
 cd "$WORKING_DIR"
+
+PREBUILT_COMPILER_ROOT="prebuilts/gcc/freebsd-x86/mips"
+PREBUILT_COMPILER_PATH="$PREBUILT_COMPILER_ROOT/mips64-unknown-freebsd"
+rm -rf "$PREBUILT_COMPILER_PATH"
+mkdir -p "$PREBUILT_COMPILER_ROOT"
+ln -s "$CHERIBSD_SDK" "$PREBUILT_COMPILER_PATH" 
+
+
 . build/envsetup.sh || exit
 lunch aosp_cheri-eng || exit
 mmma art
