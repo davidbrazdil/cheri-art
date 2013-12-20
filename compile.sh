db1519e447beda9c1cd0f1a7d187025f51028fe6
David Brazdil <david.brazdil@gmail.com>
2013-12-20 10:41:05 +0000

    Selects CHERI+FreeBSD combo

--- compile.sh
+++ compile.sh
@@ -4,8 +4,10 @@ WORKING_DIR="work"
 export PATH="$PATH":~/apps/openjdk7/usr/local/openjdk7/bin/
 export EXPERIMENTAL_USE_JAVA7=true
 
+export TARGET_OS=freebsd
+
 cd "$WORKING_DIR"
 . build/envsetup.sh || exit
-lunch aosp_mips-eng || exit
+lunch aosp_cheri-eng || exit
 mmma art
 
