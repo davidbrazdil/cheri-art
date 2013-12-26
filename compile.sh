5cab712362f87f0ed06b91113e496e41c8ac9636
David Brazdil <david.brazdil@gmail.com>
2013-12-26 10:04:49 +0000

    Forked Android's LLVM

--- compile.sh
+++ compile.sh
@@ -9,7 +9,7 @@ export TARGET_OS=freebsd
 
 cd "$DIR_ANDROID"
 
-PREBUILT_COMPILER_ROOT="prebuilts/gcc/freebsd-x86/mips"
+PREBUILT_COMPILER_ROOT="prebuilts/gcc/freebsd-x86/mips64"
 PREBUILT_COMPILER_PATH="$PREBUILT_COMPILER_ROOT/mips64-unknown-freebsd"
 rm -rf "$PREBUILT_COMPILER_PATH"
 mkdir -p "$PREBUILT_COMPILER_ROOT"
@@ -17,6 +17,6 @@ ln -s "$DIR_CHERISDK"/out "$PREBUILT_COMPILER_PATH"
 
 
 . build/envsetup.sh || exit
-lunch aosp_mips-eng || exit
+lunch cheri_mips-eng || exit
 mmma art
 
