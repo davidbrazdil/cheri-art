326624e998eda4e6798415e385f2fa4f7e183cfe
David Brazdil <david.brazdil@gmail.com>
2013-12-26 10:24:07 +0000

    Script for switching to the 'cheri' branch in forked repos

---
+++ set_repo_origin.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source config.sh
+
+set_origin()
+{
+    cd "$DIR_ANDROID"/"$1"
+    try_to_run git remote add github "$2"
+    try_to_run git checkout -b cheri
+    try_to_run git pull github cheri
+    try_to_run git push --set-upstream github cheri
+}
+
+set_origin \
+  build \
+  git@github.com:davidbrazdil/cheri-art_build.git
+set_origin \
+  external/llvm \
+  git@github.com:davidbrazdil/cheri-art_llvm.git
+
