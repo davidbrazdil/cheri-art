a95e401a99776be826e7136d98ac5463b7cd29e6
David Brazdil <david.brazdil@gmail.com>
2014-02-08 20:26:11 +0000

    Adds GNU AR into prebuilt clang

--- compile.sh
+++ compile.sh
@@ -20,6 +20,9 @@ rm -rf "$PREBUILT_COMPILER_PATH"
 mkdir -p "$PREBUILT_COMPILER_ROOT"
 ln -s "$DIR_CHERISDK"/sdk "$PREBUILT_COMPILER_PATH" 
 
+AR_REPLACEMENT="$PREBUILT_COMPILER_PATH"/bin/mips4-unknown-freebsd-ar
+rm -f "$AR_REPLACEMENT"
+ln -s "$AR" "$AR_REPLACEMENT"
 
 . build/envsetup.sh || exit
 lunch cheri_mips-eng || exit
